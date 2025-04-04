cff2e479e9eef14fdf31eace2ecd619fc66107e9
v.kaukin <v.kaukin@u6.ru>
2025-04-04 13:41:55 +0500

    docs

--- concurrency.py
+++ concurrency.py
@@ -1,6 +1,32 @@
 from urllib import request
 import time
 
+"""
+16 cores, 24 logic cores
+
+30 urls:
+    sync_req        : 8.27 sec
+    proc_pool 10    : 1.12 sec
+    thread_pool 10  : 1.11 sec
+    aio_loop_sync   : 3.62 sec
+    aio_loop_gather : 0.47 sec
+
+100 urls:
+    sync_req        : 31.57 sec
+    proc_pool 20    :  1.81 sec
+    thread_pool 20  :  2.00 sec
+    aio_loop_sync   : 11.01 sec
+    aio_loop_gather :  0.65 sec
+    
+500 urls:
+    proc_pool 30    :  7.56 sec
+    thread_pool 30  :  7.77 sec
+    aio_loop_sync   : 47.31 sec
+    aio_loop_gather :  0.96 sec
+"""
+
+URL_SET = ['https://ya.ru/?q={}'.format(i) for i in range(500)]  # https://duckduckgo.com/?q={}
+
 
 def _timeit(fn):
     t = time.time()
@@ -20,39 +46,26 @@ def _sync_request_url(url: str) -> bool:
         return False
 
 
-URL_SET = ['https://duckduckgo.com/?q={}'.format(i) for i in range(30)]
-statistic_30_urls = """
-host: 4 cores 3.4Ghz, 16Gb ram, 11.09 МБайт/с IN, 4.10 МБайт/с OUT
+def sync_req():
+    for url in URL_SET:
+        print(_sync_request_url(url))
 
-proc_pool
-10 = 2.25 sec
-5 = 3.08 sec
-1 = 13.7 sec
-
-thread_pool
-10 = 1.63 sec
-5 = 2.92 sec
-1 = 13.03 sec
-
-aio_loop
-3.72 sec
-"""
 
 def proc_pool():
     from multiprocessing import Pool
-    with Pool(1) as p:
+    with Pool(20) as p:
         res = p.map(_sync_request_url, URL_SET)
         print(len(res), 'ok')
 
 
 def thread_pool():
     from multiprocessing.pool import ThreadPool
-    with ThreadPool(1) as p:
+    with ThreadPool(20) as p:
         res = p.map(_sync_request_url, URL_SET)
         print(len(res), 'ok')
 
 
-def aio_loop():
+def aio_loop_sync():
     import aiohttp
     import asyncio
     async def main():
@@ -69,7 +82,25 @@ def aio_loop():
     loop.run_until_complete(main())
 
 
+def aio_loop_gather():
+    import asyncio
+    import aiohttp
+    import time
+
+    async def fetch_url(session, url):
+        async with session.get(url) as response:
+            html = await response.text()
+            print(f"{time.time():.2f} | {url} | {len(html)} bytes")
+
+    async def main():
+        async with aiohttp.ClientSession() as session:
+            tasks = [fetch_url(session, url) for url in URL_SET]
+            await asyncio.gather(*tasks)  # *параллельное выполнение
+
+    asyncio.run(main())  # Современный способ запуска
+
+
 if __name__ == '__main__':
     _timeit(
-        aio_loop
+        aio_loop_gather
     )
